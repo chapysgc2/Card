4597aab9931f49410dabf7786d4d3c5d591afdb5
Hazel Alain <hazelo235@gmail.com>
2023-11-16 00:47:32 -0600

    Initial Commit

---
+++ Card/CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApp.swift
+//  Card
+//
+//  Created by Hazel Alain on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
